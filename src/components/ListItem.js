cc1f1f86ef7142a0c1b233f38336aef31760cc3a
mike-nyange <micknyange@gmail.com>
2022-07-15 21:29:51 +0300

    First commit

---
+++ src/components/ListItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListItem = (props) => {
+    console.log('PROPS:', props)
+  return (
+    <div>
+        <h3>ListItem</h3>
+    </div>
+  )
+}
+
+export default ListItem
---
+++ src/pages/NotesListPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import notes from '../assets/data'
+import ListItem from '../components/ListItem'
+
+const NotesListPage = () => {
+  return (
+    <div>
+        <div className="notes-list">
+            {notes.map(note => (
+                <ListItem />
+            ))}
+        </div>
+    </div>
+  )
+}
+
+export default NotesListPage
